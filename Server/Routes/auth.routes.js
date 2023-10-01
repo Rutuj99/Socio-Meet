a691d34de081ad809c9994236956b740550295d2
Rutuj99 <rutujbg99@gmail.com>
2023-10-01 12:59:40 +0530

    day2

---
+++ Server/Routes/auth.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+let authRoutes=express.Router();
+
+
+
+authRoutes("/register",(req,res)=>{
+        
+})
+
+
+
+authRoutes("/login",(req,res)=>{
+       
+})
+
+
+
+authRoutes("/allreadyLogin",(req,res)=>{
+     
+})
